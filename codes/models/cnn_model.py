39bdd06da10b79a86840f58f30144d45836be204
Snigdha1078 <snig2018narayan@gmail.com>
2023-07-30 16:34:28 +1000

    refactor: adjust to match output of conv layer

--- codes/models/cnn_model.py
+++ codes/models/cnn_model.py
@@ -1,3 +1,4 @@
+# cnn_model.py
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -5,20 +6,20 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.conv3 = nn.Conv2d(16, 32, 3)
-        self.conv4 = nn.Conv2d(32, 64, 3)
-        self.fc1 = nn.Linear(64 * 2 * 2, 120)
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64 * 8 * 8, 120)  # Adjusted to match the output of the conv layers
         self.fc2 = nn.Linear(120, 2)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
+        x = F.relu(self.conv1(x))
         x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        x = F.relu(self.conv3(x))
         x = self.pool(F.relu(self.conv4(x)))
-        x = x.view(-1, 64 * 2 * 2)
+        x = x.view(x.size(0), -1)  # Flatten the tensor
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
